311862b6ae4b933ddb3bab50d2b8f1156664090d
Jakša Tomović <jaksa.tomovic@gmail.com>
2017-03-17 10:12:18 +0100

    Add files via upload

---
+++ Snapgram/navVC.swift
@@ -0,0 +1,36 @@
+//
+//  navVC.swift
+//  Snapgram
+//
+//  Created by Jaksa Tomovic on 28/11/16.
+//  Copyright © 2016 Jaksa Tomovic. All rights reserved.
+//
+
+import UIKit
+
+class navVC: UINavigationController {
+    
+    // default func
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // color of title at the top in nav controller
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
+        
+        // color of buttons in nav controller
+        self.navigationBar.tintColor = .white
+        
+        // color of background of nav controller
+        self.navigationBar.barTintColor = UIColor(red: 18.0 / 255.0, green: 86.0 / 255.0, blue: 136.0 / 255.0, alpha: 1)
+        
+        // disable translucent
+        self.navigationBar.isTranslucent = false
+    }
+    
+    
+    // white status bar function
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+
+}
---
+++ Snapgram/pictureCell.swift
@@ -0,0 +1,26 @@
+//
+//  pictureCell.swift
+//  Snapgram
+//
+//  Created by Jaksa Tomovic on 28/11/16.
+//  Copyright © 2016 Jaksa Tomovic. All rights reserved.
+//
+
+import UIKit
+
+class pictureCell: UICollectionViewCell {
+    
+    // holds post picture
+    @IBOutlet weak var picImg: UIImageView!
+    
+    
+    // default func
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // alignment
+        let width = UIScreen.main.bounds.width
+        picImg.frame = CGRect(x: 0, y: 0, width: width / 3, height: width / 3)
+    }
+    
+}
